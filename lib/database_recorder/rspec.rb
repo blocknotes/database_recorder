914081b224e1935639770453da0338e89c50f2ec
Mattia Roccoberton <mattia.roccoberton@treatwell.com>
2022-04-14 22:40:48 +0200

    Metadata setup

--- lib/database_recorder/rspec.rb
+++ lib/database_recorder/rspec.rb
@@ -16,6 +16,7 @@ module DatabaseRecorder
           options.merge!(example.metadata[:dbr]) if example.metadata[:dbr].is_a?(Hash)
           options.merge!(example: example, name: "#{example.full_description}__#{ref}")
           Recording.new(options: options).tap do |recording|
+            recording.metadata = { 'example' => example.id, 'started_at' => Time.now }
             result = recording.start { example.run }
             if options[:verify_queries] && result[:stored_queries]
               expect(result[:stored_queries]).to match_array(result[:current_queries])
